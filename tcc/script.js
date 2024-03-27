bf2c80438b327d82947510a3374a25b0ce8f7ece
xxxlucasmatheus <117852180+xxxlucasmatheus@users.noreply.github.com>
2024-03-27 14:28:29 -0300

    Add files via upload

---
+++ tcc/script.js
@@ -0,0 +1,7 @@
+function pagar() {
+    // Simulação de pagamento com PIX (poderia ser uma chamada para uma API real)
+    setTimeout(() => {
+        // Exibir mensagem de confirmação após 2 segundos
+        document.getElementById('confirmacao').style.display = 'block';
+    }, 2000);
+}
